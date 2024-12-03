9b6c864f35d935994ef91b6b9d6e91ea5c188738
Alex Lay-Calvert <alex.laycalvert@gmail.com>
2024-12-03 09:11:31 -0500

    2024 day 3

--- go/years/year2024/day3.go
+++ go/years/year2024/day3.go
@@ -1,13 +1,184 @@
 package year2024
 
+import (
+	"strconv"
+)
+
+// ANSWERS:
+//
+// Part 1: 174561379
+//
+// Part 2: 106921067
 type Day3 struct {
-    Input []string
+	Input []string
 }
 
 func (d Day3) Part1() string {
-    return "Not Implemented"
+	sum := 0
+
+	for _, line := range d.Input {
+		sum += parseLine(line)
+	}
+
+	return strconv.Itoa(sum)
 }
 
 func (d Day3) Part2() string {
-    return "Not Implemented"
+	sum := 0
+	isEnabled := true
+
+	for _, line := range d.Input {
+		product, newIsEnabled := parseLineWithConditionals(line, isEnabled)
+		isEnabled = newIsEnabled
+		sum += product
+	}
+
+	return strconv.Itoa(sum)
+}
+
+func parseLine(line string) int {
+	i := 0
+	sum := 0
+	for {
+		if i >= len(line) {
+			break
+		}
+		char := line[i]
+		switch char {
+		// start of possible `mul` instruction
+		case 'm':
+			product, newI, ok := parseMul(line, i)
+			if ok {
+				sum += product
+			}
+			i = newI
+			break
+		default:
+			i++
+			break
+		}
+	}
+	return sum
+}
+
+func parseLineWithConditionals(line string, enabled bool) (int, bool) {
+	i := 0
+	sum := 0
+	for {
+		if i >= len(line) {
+			break
+		}
+		char := line[i]
+		switch char {
+		// start of possible conditional `do` or `don't`
+		case 'd':
+			shouldEnable, newI, ok := parseConditional(line, i)
+			if ok {
+				enabled = shouldEnable
+			}
+			i = newI
+			break
+		// start of possible `mul` instruction
+		case 'm':
+			if !enabled {
+				i++
+				break
+			}
+			product, newI, ok := parseMul(line, i)
+			if ok {
+				sum += product
+			}
+			i = newI
+			break
+		default:
+			i++
+			break
+		}
+	}
+	return sum, enabled
+}
+
+// parseMul attempts to parse a `mul` instruction in the `line`
+// starting at the `start` index. Assumes that `line[start] == 'm'`.
+//
+// If a `mul` instruction is found, the function returns the product
+// of the two numbers, the index of the character after the closing
+// parenthesis, and true.
+//
+// Otherwise, the function returns 0, the index of the failed token (new
+// start index), and false.
+func parseMul(line string, start int) (int, int, bool) {
+	ok, start := checkWord(line, start, "mul(")
+	if !ok {
+		return 0, start, false
+	}
+
+	// parse first number
+	num1, start, ok := parseNumber(line, start)
+	if !ok {
+		return 0, start, false
+	}
+
+	// parse comma
+	if !checkChar(line, start, ',') {
+		return 0, start, false
+	}
+	start++
+
+	// parse second number
+	num2, start, ok := parseNumber(line, start)
+	if !ok {
+		return 0, start, false
+	}
+
+	// parse closing parenthesis
+	if !checkChar(line, start, ')') {
+		return 0, start, false
+	}
+
+	return num1 * num2, start + 1, true
+}
+
+func checkChar(line string, i int, char byte) bool {
+	return i < len(line) && line[i] == char
+}
+
+func checkWord(line string, start int, word string) (bool, int) {
+	for i, char := range word {
+		if !checkChar(line, start+i, byte(char)) {
+			return false, start + i
+		}
+	}
+	return true, start + len(word)
+}
+
+func parseNumber(line string, start int) (int, int, bool) {
+	numStr := ""
+	for {
+		if start >= len(line) {
+			break
+		}
+		char := line[start]
+		if char < '0' || char > '9' {
+			break
+		}
+		numStr += string(char)
+		start++
+	}
+	num, err := strconv.Atoi(numStr)
+	return num, start, err == nil
+}
+
+func parseConditional(line string, start int) (bool, int, bool) {
+	ok, doStart := checkWord(line, start, "do()")
+	if ok {
+		return true, doStart, true
+	}
+
+	ok, dontStart := checkWord(line, start, "don't()")
+	if ok {
+		return false, dontStart, true
+	}
+
+	return false, start, false
 }
--- go/years/year2024/year2024_test.go
+++ go/years/year2024/year2024_test.go
@@ -1,1 +1,71 @@
-package year2024
+package year2024_test
+
+import (
+	"testing"
+
+	"github.com/alex-laycalvert/advent-of-code/years/year2024"
+)
+
+func TestDay1(t *testing.T) {
+	day1 := year2024.Day1{Input: []string{
+		"3   4",
+		"4   3",
+		"2   5",
+		"1   3",
+		"3   9",
+		"3   3",
+	}}
+
+	expectedPart1 := "11"
+	part1 := day1.Part1()
+	if part1 != expectedPart1 {
+		t.Fatalf("Expected %s, got %s", expectedPart1, part1)
+	}
+
+	expectedPart2 := "31"
+	part2 := day1.Part2()
+	if part2 != expectedPart2 {
+		t.Fatalf("Expected %s, got %s", expectedPart2, part2)
+	}
+}
+
+func TestDay2(t *testing.T) {
+	day2 := year2024.Day2{Input: []string{
+		"7 6 4 2 1",
+		"1 2 7 8 9",
+		"9 7 6 2 1",
+		"1 3 2 4 5",
+		"8 6 4 4 1",
+		"1 3 6 7 9",
+	}}
+
+	expectedPart1 := "2"
+	part1 := day2.Part1()
+	if part1 != expectedPart1 {
+		t.Fatalf("Expected %s, got %s", expectedPart1, part1)
+	}
+
+	expectedPart2 := "4"
+	part2 := day2.Part2()
+	if part2 != expectedPart2 {
+		t.Fatalf("Expected %s, got %s", expectedPart2, part2)
+	}
+}
+
+func TestDay3(t *testing.T) {
+	day3 := year2024.Day3{Input: []string{
+		"xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))",
+	}}
+
+	expectedPart1 := "161"
+	part1 := day3.Part1()
+	if part1 != expectedPart1 {
+		t.Fatalf("Expected %s, got %s", expectedPart1, part1)
+	}
+
+	expectedPart2 := "48"
+	part2 := day3.Part2()
+	if part2 != expectedPart2 {
+		t.Fatalf("Expected %s, got %s", expectedPart2, part2)
+	}
+}
