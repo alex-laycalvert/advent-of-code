142c20d1a678c601276b216ab68c1e5f8f74c114
Alex Lay-Calvert <alex.laycalvert@gmail.com>
2024-12-04 15:09:30 -0500

    2023 day 2

--- go/years/year2023/day2.go
+++ go/years/year2023/day2.go
@@ -7,25 +7,86 @@ import (
 	"github.com/alex-laycalvert/advent-of-code/utils"
 )
 
+// ANSWERS:
+//
+// Part 1: 2076
+//
+// Part 2: 70950
 type Day2 struct {
 	Input []string
 }
 
 func (d Day2) Part1() string {
+	totalColors := map[string]int{
+		"red":   12,
+		"green": 13,
+		"blue":  14,
+	}
+
 	sum := 0
 	for _, line := range d.Input {
-		parts := strings.Split(line, ":")
-		_, err := strconv.Atoi(parts[0][5:])
+		selections := strings.Split(line, ":")
+		gameID, err := strconv.Atoi(selections[0][5:])
 		utils.PanicErr(err)
 
-		parts = strings.Split(parts[1], ";")
-		for _, part := range parts {
-			_ = strings.Split(strings.TrimSpace(part), ",")
+		selections = strings.Split(selections[1], ";")
+		isPossible := true
+		for _, part := range selections {
+			marbles := strings.Split(strings.TrimSpace(part), ",")
+			for _, marble := range marbles {
+				parts := strings.Split(strings.TrimSpace(marble), " ")
+				count, err := strconv.Atoi(parts[0])
+				utils.PanicErr(err)
+				color := parts[1]
+				if total, _ := totalColors[color]; count > total {
+					isPossible = false
+					break
+				}
+			}
+
+			if !isPossible {
+				break
+			}
+		}
+		if isPossible {
+			sum += gameID
 		}
 	}
 	return strconv.Itoa(sum)
 }
 
 func (d Day2) Part2() string {
-	return "Not Implemented"
+	minColors := map[string]int{
+		"red":   0,
+		"green": 0,
+		"blue":  0,
+	}
+
+	sum := 0
+	for _, line := range d.Input {
+		selections := strings.Split(line, ":")
+		selections = strings.Split(selections[1], ";")
+		for _, part := range selections {
+			marbles := strings.Split(strings.TrimSpace(part), ",")
+			for _, marble := range marbles {
+				parts := strings.Split(strings.TrimSpace(marble), " ")
+				count, err := strconv.Atoi(parts[0])
+				utils.PanicErr(err)
+				color := parts[1]
+				if currentMin, _ := minColors[color]; count > currentMin {
+					minColors[color] = count
+				}
+			}
+		}
+
+		numRed, _ := minColors["red"]
+		numGreen, _ := minColors["green"]
+		numBlue, _ := minColors["blue"]
+		sum += numRed * numGreen * numBlue
+
+		minColors["red"] = 0
+		minColors["green"] = 0
+		minColors["blue"] = 0
+	}
+	return strconv.Itoa(sum)
 }
--- go/years/year2023/year2023_test.go
+++ go/years/year2023/year2023_test.go
@@ -38,21 +38,21 @@ func TestDay1(t *testing.T) {
 	}
 }
 
-// func TestDay2(t *testing.T) {
-// 	day2 := year2023.Day2{Input: []string{
-// 		"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
-// 		"Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
-// 		"Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
-// 		"Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
-// 		"Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
-// 	}}
-// 	expected := "8"
-// 	if actual := day2.Part1(); actual != expected {
-// 		t.Errorf("Expected %s but got %s", expected, actual)
-// 	}
-//
-// 	expected = "2286"
-// 	if actual := day2.Part2(); actual != expected {
-// 		t.Errorf("Expected %s but got %s", expected, actual)
-// 	}
-// }
+func TestDay2(t *testing.T) {
+	day2 := year2023.Day2{Input: []string{
+		"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+		"Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+		"Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+		"Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+		"Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+	}}
+	expected := "8"
+	if actual := day2.Part1(); actual != expected {
+		t.Errorf("Expected %s but got %s", expected, actual)
+	}
+
+	expected = "2286"
+	if actual := day2.Part2(); actual != expected {
+		t.Errorf("Expected %s but got %s", expected, actual)
+	}
+}
