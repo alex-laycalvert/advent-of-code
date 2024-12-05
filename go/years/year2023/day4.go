8f5cf742b1d1b9f9a8c39cda472adea78926c720
Alex Lay-Calvert <alex.laycalvert@gmail.com>
2024-12-04 20:44:19 -0500

    2023 day 4 part 1 and 2

--- go/years/year2023/day4.go
+++ go/years/year2023/day4.go
@@ -1,13 +1,244 @@
 package year2023
 
+import (
+	"slices"
+	"strconv"
+)
+
+// ANSWERS:
+//
+// Part 1: 32001
+//
+// Part 2: 5037841
 type Day4 struct {
 	Input []string
 }
 
 func (d Day4) Part1() string {
-	return "Not Implemented"
+	return d.part1_sliceIncludes()
+	// return d.part1_mapOnEachLine()
+	// return d.part1_globalMap()
+}
+
+// on my laptop, avg time per iter is ~0.41ms over 100k iters
+// Each slice isn't more than maybe 8 items or so so it's not
+// very resource intensive to just iterate over the slice.
+func (d *Day4) part1_sliceIncludes() string {
+	sum := 0
+
+	firstNumIndex := 0
+	if len(d.Input) < 10 {
+		firstNumIndex = 8
+	} else {
+		firstNumIndex = 10
+	}
+
+	for _, line := range d.Input {
+		winningNums, lastIndex := parseNumbersToSlice(line, firstNumIndex)
+		playingNums, _ := parseNumbersToSlice(line, lastIndex+1)
+		gamePoints := 0
+		for _, num := range playingNums {
+			if !slices.Contains(winningNums, num) {
+				continue
+			}
+			if gamePoints == 0 {
+				gamePoints = 1
+				continue
+			}
+			gamePoints *= 2
+		}
+		sum += gamePoints
+	}
+
+	return strconv.Itoa(sum)
+}
+
+// ~0.5ms / iter over 100k iters
+// Probably marginally slower because a map is being intialized on each
+// line which is slower than a slice.
+func (d *Day4) part1_mapOnEachLine() string {
+	sum := 0
+
+	firstNumIndex := 0
+	if len(d.Input) < 10 {
+		firstNumIndex = 8
+	} else {
+		firstNumIndex = 10
+	}
+
+	for _, line := range d.Input {
+		winningNums, lastIndex := parseNumbersToNewMap(line, firstNumIndex)
+		playingNums, _ := parseNumbersToSlice(line, lastIndex+1)
+		gamePoints := 0
+		for _, num := range playingNums {
+			if _, ok := winningNums[num]; !ok {
+				continue
+			}
+			if gamePoints == 0 {
+				gamePoints = 1
+				continue
+			}
+			gamePoints *= 2
+		}
+		sum += gamePoints
+	}
+
+	return strconv.Itoa(sum)
+}
+
+// ~0.43ms / iter over 100k iters
+// Clearing the map on each call is probably the most expensive part
+func (d *Day4) part1_globalMap() string {
+	sum := 0
+
+	firstNumIndex := 0
+	if len(d.Input) < 10 {
+		firstNumIndex = 8
+	} else {
+		firstNumIndex = 10
+	}
+
+	winningNums := make(map[int]int)
+	for _, line := range d.Input {
+		lastIndex := parseNumbersToExistingMap(line, firstNumIndex, winningNums)
+		playingNums, _ := parseNumbersToSlice(line, lastIndex+1)
+		gamePoints := 0
+		for _, num := range playingNums {
+			if _, ok := winningNums[num]; !ok {
+				continue
+			}
+			if gamePoints == 0 {
+				gamePoints = 1
+				continue
+			}
+			gamePoints *= 2
+		}
+		sum += gamePoints
+	}
+
+	return strconv.Itoa(sum)
 }
 
 func (d Day4) Part2() string {
-	return "Not Implemented"
+	sum := 0
+
+	firstNumIndex := 0
+	if len(d.Input) < 10 {
+		firstNumIndex = 8
+	} else {
+		firstNumIndex = 10
+	}
+
+	copies := make(map[int]int)
+
+	for i, line := range d.Input {
+		winningNums, lastIndex := parseNumbersToSlice(line, firstNumIndex)
+		playingNums, _ := parseNumbersToSlice(line, lastIndex+1)
+
+		gamePoints := 0
+		copiesOfThisGame := copies[i]
+		for _, num := range playingNums {
+			if !slices.Contains(winningNums, num) {
+				continue
+			}
+			gamePoints++
+			copies[i+gamePoints] += copiesOfThisGame + 1
+		}
+		sum += copiesOfThisGame + 1
+	}
+
+	return strconv.Itoa(sum)
+}
+
+func parseNumbersToSlice(line string, start int) ([]int, int) {
+	nums := []int{}
+	currentNumStr := ""
+	lastIndex := 0
+	for i, ch := range line[start:] {
+		if ch == '|' {
+			lastIndex = start + i
+			break
+		}
+
+		if ch == ' ' {
+			if currentNumStr == "" {
+				continue
+			}
+			num, err := strconv.Atoi(currentNumStr)
+			if err == nil {
+				nums = append(nums, num)
+			}
+			currentNumStr = ""
+			continue
+		}
+
+		currentNumStr += string(ch)
+	}
+	num, err := strconv.Atoi(currentNumStr)
+	if err == nil {
+		nums = append(nums, num)
+	}
+	return nums, lastIndex
+}
+
+func parseNumbersToNewMap(line string, start int) (map[int]int, int) {
+	nums := make(map[int]int)
+	currentNumStr := ""
+	lastIndex := 0
+	for i, ch := range line[start:] {
+		if ch == '|' {
+			lastIndex = start + i
+			break
+		}
+
+		if ch == ' ' {
+			if currentNumStr == "" {
+				continue
+			}
+			num, err := strconv.Atoi(currentNumStr)
+			if err == nil {
+				nums[num] = num
+			}
+			currentNumStr = ""
+			continue
+		}
+
+		currentNumStr += string(ch)
+	}
+	num, err := strconv.Atoi(currentNumStr)
+	if err == nil {
+		nums[num] = num
+	}
+	return nums, lastIndex
+}
+
+func parseNumbersToExistingMap(line string, start int, nums map[int]int) int {
+	clear(nums)
+	currentNumStr := ""
+	lastIndex := 0
+	for i, ch := range line[start:] {
+		if ch == '|' {
+			lastIndex = start + i
+			break
+		}
+
+		if ch == ' ' {
+			if currentNumStr == "" {
+				continue
+			}
+			num, err := strconv.Atoi(currentNumStr)
+			if err == nil {
+				nums[num] = num
+			}
+			currentNumStr = ""
+			continue
+		}
+
+		currentNumStr += string(ch)
+	}
+	num, err := strconv.Atoi(currentNumStr)
+	if err == nil {
+		nums[num] = num
+	}
+	return lastIndex
 }
--- go/years/year2023/year2023_test.go
+++ go/years/year2023/year2023_test.go
@@ -46,6 +46,7 @@ func TestDay2(t *testing.T) {
 		"Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
 		"Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
 	}}
+
 	expected := "8"
 	if actual := day2.Part1(); actual != expected {
 		t.Errorf("Expected %s but got %s", expected, actual)
@@ -77,6 +78,27 @@ func TestDay3(t *testing.T) {
 
 	expected = "467835"
 	if actual := day3.Part2(); actual != expected {
+		t.Errorf("Expected %s but got %s", expected, actual)
+	}
+}
+
+func TestDay4(t *testing.T) {
+	day4 := year2023.Day4{Input: []string{
+		"Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+		"Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
+		"Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
+		"Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
+		"Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
+		"Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11",
+	}}
+
+	expected := "13"
+	if actual := day4.Part1(); actual != expected {
+		t.Errorf("Expected %s but got %s", expected, actual)
+	}
+
+	expected = "30"
+	if actual := day4.Part2(); actual != expected {
 		t.Errorf("Expected %s but got %s", expected, actual)
 	}
 }
